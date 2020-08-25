e11e7f274a178be5c07d12425731469183c0ea66
foxrock619 <foxrock619@gmail.com>
2020-08-25 16:37:41 -0500

    added comment

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function curDate() {
+    var curdate = new Date();
+    var year = mydate.getYear();
+    var day = mydate.getDay();
+    var month = mydate.getMonth();
+    var daym = mydate.getDate();
+    var dayarray = new Array()
+}
