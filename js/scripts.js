d7e9d6b1c3e5c78992b3944520360e7b62f30351
foxrock619 <foxrock619@gmail.com>
2020-09-05 18:39:30 -0500

    09/05 update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function curDate() {
+﻿function curDate() {
     var curdate = new Date();
     var year = mydate.getYear();
     var day = mydate.getDay();
@@ -6,9 +6,33 @@ function curDate() {
     var daym = mydate.getDate();
     var dayarray = new Array()
 }
-function PlaySound(melody) {
-    alert("On Press of " + melody);
-    var path = "../music2.mp3"
-    var snd = new Audio(path + melody + ".mp3");
-    snd.play();
-}
+var ap = document.getElementById('ap');
+
+setTimeout(function () {
+    ap.style.display = 'none';
+}, 5000);
+
+// Set the date we're counting down to
+var countDownDate = new Date("May 27, 2020 9:37:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = now - countDownDate;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = "相识 " + days + " 天 " + hours + " 小时 "
+        + minutes + " 分钟 " + seconds + " 秒 ";
+
+
+}, 1000);
